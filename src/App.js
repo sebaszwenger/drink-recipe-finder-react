b46e460a7cdca8428f2342393e8288edb7c320f7
sebaszwenger <sebaszwenger@gmail.com>
2021-10-19 19:39:44 -0300

    call ListRecipes component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Form from "./components/Form";
 import Header from "./components/Header";
+import ListRecipes from "./components/ListRecipes";
 import CategoryProvider from "./context/CategoryContext";
 import RecipesProvider from "./context/RecipesContext";
 
@@ -13,6 +14,7 @@ function App() {
           <div className="row">
             <Form />
           </div>
+          <ListRecipes />
         </div>
       </RecipesProvider>
     </CategoryProvider>
