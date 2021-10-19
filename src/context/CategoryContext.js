6b326ec74c0885c0003abfbee159a7fbadb313d8
sebaszwenger <sebaszwenger@gmail.com>
2021-10-19 12:48:43 -0300

    create new useContext CategoryContext with the categorys of thecocktaildb API

---
+++ src/context/CategoryContext.js
@@ -0,0 +1,31 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+//Create context
+export const CategoryContext = createContext();
+
+//Provider
+const CategoryProvider = (props) => {
+  //create state
+  const [categorys, setCategorys] = useState([]);
+
+  //Call API
+  useEffect(() => {
+    const getCategory = async () => {
+      const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list";
+
+      const category = await axios.get(url);
+
+      setCategorys(category.data.drinks);
+    };
+
+    getCategory();
+  }, []);
+
+  return (
+    <CategoryContext.Provider value={{ categorys }}>
+      {props.children}
+    </CategoryContext.Provider>
+  );
+};
+
+export default CategoryProvider;
