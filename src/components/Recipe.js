bdbf548edc823760cd2e671766c210516ba2f5ee
sebaszwenger <sebaszwenger@gmail.com>
2021-10-19 19:38:31 -0300

    new component to show a recipe

---
+++ src/components/Recipe.js
@@ -0,0 +1,11 @@
+const Recipe = ({ recipe }) => {
+  return (
+    <div className="col-md-4 mb-3">
+      <div className="card">
+        <h2 className="card-header">{recipe.strDrink}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Recipe;
