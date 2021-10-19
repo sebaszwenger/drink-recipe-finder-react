223c129a430dd005d82faa193556a4c898b825a8
sebaszwenger <sebaszwenger@gmail.com>
2021-10-19 19:38:06 -0300

    new component to show the list of recipes

---
+++ src/components/ListRecipes.js
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+
+import { RecipesContext } from "../context/RecipesContext";
+import Recipe from "./Recipe";
+
+const ListRecipes = () => {
+  const { recipes } = useContext(RecipesContext);
+  return (
+    <div className="row mt-5">
+      {recipes.map((recipe) => (
+        <Recipe key={recipe.idDrink} recipe={recipe} />
+      ))}
+    </div>
+  );
+};
+
+export default ListRecipes;
