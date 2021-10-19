0c73ad5dd7c5838d1f8e5d031eeaa9b9c490b1af
sebaszwenger <sebaszwenger@gmail.com>
2021-10-19 13:34:30 -0300

    new "search" state to store inputs. new "error" state to validate empty inputs. import useContext. add categories by Select. pass search terms to the RecipesContext

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,45 @@
+import { useContext, useState } from "react";
+import { CategoryContext } from "../context/CategoryContext";
+import { RecipesContext } from "../context/RecipesContext";
+
+import Error from "./Error";
+
 const Form = () => {
+  const [search, setSearch] = useState({
+    name: "",
+    category: "",
+  });
+  const { name, category } = search;
+
+  const [error, setError] = useState(false);
+
+  //Extract functions from useContexts
+  const { categorys } = useContext(CategoryContext);
+  const { searchRecipes } = useContext(RecipesContext);
+
+  //function to save the form data
+  const getDataRecipe = (e) => {
+    e.preventDefault();
+    if (name.trim() === "" || category.trim() === "") {
+      setError(true);
+      return;
+    }
+    setError(false);
+    searchRecipes(search);
+  };
+
+  const updateState = (e) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
-    <form className="col-12">
-      <fildset className="text-center">
+    <form className="col-12" onSubmit={getDataRecipe}>
+      <fieldset className="text-center">
         <legend>Search Drinks by Category or by Ingredient</legend>
-      </fildset>
+      </fieldset>
 
       <div className="row mt-4">
         <div className="col-md-4">
@@ -12,6 +48,7 @@ const Form = () => {
             type="text"
             className="form-control"
             placeholder="Search by Ingredient"
+            onChange={updateState}
           />
         </div>
 
@@ -20,8 +57,17 @@ const Form = () => {
             className="form-control"
             name="category"
             placeholder="Category"
+            onChange={updateState}
           >
-            <option value="">-- Select Category --</option>
+            <option value="" hidden>
+              -- Select Category --
+            </option>
+
+            {categorys.map((category) => (
+              <option key={category.strCategory} value={category.strCategory}>
+                {category.strCategory}
+              </option>
+            ))}
           </select>
         </div>
         <div className="col-md-4">
@@ -32,6 +78,7 @@ const Form = () => {
           />
         </div>
       </div>
+      {error ? <Error message="Todos los Campos son Obligatorios" /> : null}
     </form>
   );
 };
