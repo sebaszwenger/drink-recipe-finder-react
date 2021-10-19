f2042d562a0d48605844418f140b1d7e63faa6db
sebaszwenger <sebaszwenger@gmail.com>
2021-10-19 13:23:25 -0300

    Providers imported and implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import Form from "./components/Form";
 import Header from "./components/Header";
+import CategoryProvider from "./context/CategoryContext";
+import RecipesProvider from "./context/RecipesContext";
 
 function App() {
   return (
-    <div>
-      <Header />
+    <CategoryProvider>
+      <RecipesProvider>
+        <Header />
 
-      <div className="container mt-5">
-        <div className="row">
-          <Form />
+        <div className="container mt-5">
+          <div className="row">
+            <Form />
+          </div>
         </div>
-      </div>
-    </div>
+      </RecipesProvider>
+    </CategoryProvider>
   );
 }
 
