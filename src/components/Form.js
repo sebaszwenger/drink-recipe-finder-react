6d31cd41d68d4e3b7e0bae4c3aa1a2ffcaf17ada
sebaszwenger <sebaszwenger@gmail.com>
2021-10-18 21:23:41 -0300

    add inputs and submit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,38 @@
 const Form = () => {
   return (
-    <>
-      <h1>Form</h1>
-    </>
+    <form className="col-12">
+      <fildset className="text-center">
+        <legend>Search Drinks by Category or by Ingredient</legend>
+      </fildset>
+
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            name="name"
+            type="text"
+            className="form-control"
+            placeholder="Search by Ingredient"
+          />
+        </div>
+
+        <div className="col-md-4">
+          <select
+            className="form-control"
+            name="category"
+            placeholder="Category"
+          >
+            <option value="">-- Select Category --</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-primary btn-block"
+            value="Find Drink"
+          />
+        </div>
+      </div>
+    </form>
   );
 };
 
