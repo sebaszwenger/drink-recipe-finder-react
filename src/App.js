a0142b24e1e42c937934fab2bc4aa2d6b9b4e746
sebaszwenger <sebaszwenger@gmail.com>
2021-10-19 20:31:23 -0300

    add ModalProvider

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import Form from "./components/Form";
 import Header from "./components/Header";
 import ListRecipes from "./components/ListRecipes";
+
 import CategoryProvider from "./context/CategoryContext";
 import RecipesProvider from "./context/RecipesContext";
+import ModalProvider from "./context/ModalContext";
 
 function App() {
   return (
     <CategoryProvider>
       <RecipesProvider>
-        <Header />
-
-        <div className="container mt-5">
-          <div className="row">
-            <Form />
+        <ModalProvider>
+          <Header />
+          <div className="container mt-5">
+            <div className="row">
+              <Form />
+            </div>
+            <ListRecipes />
           </div>
-          <ListRecipes />
-        </div>
+        </ModalProvider>
       </RecipesProvider>
     </CategoryProvider>
   );
